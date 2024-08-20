4bb06b1d1a99aa1ee407167787d541ac8f882cbb
MartinezAgullo <pablo.martinez.agullo@gmail.com>
2024-08-20 16:45:24 +0200

    complete TorchCaptioner.py

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
-pip install gymnasium transformers Pillow torch torchvision torchaudio
+# GenerativeAI-based image captioner
 
 
-python3.9 AutomatedPhotoCaptioning.py
-python3.9 VisualQuestionAnswering.py
+## Simple models
 
-Using Gradio to create the web interface
 
-pip install gradio
+ - [VisualQuestionAnswering.py](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/VisualQuestionAnswering.py): Provides conditional and not-conditioned image captioning.
+ - [AutomatedPhotoCaptioning.py](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/AutomatedPhotoCaptioning.py): Provides caption for a given picture.
+ - [WebGenerateCaption.py](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/WebGenerateCaption.py): Web-based application to provide captions using `BlipProcessor` and `BlipForConditionalGeneration`.
+ - [TorchCaptioner.py](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/TorchCaptioner.py):  Web-based application to provide captions using torchvision. Returns probabilities. See Figure 2.
+
+To run them 
+>     pip install gymnasium transformers Pillow torch torchvision torchaudio
+>     pip install gradio
+>     python3.9 AutomatedPhotoCaptioning.py
+>     python3.9 VisualQuestionAnswering.py
+>     python3.9 TorchCaptioner.py
+The web interfaces are obtained using Gradio. After executing the python script, the AI tool is available in the local server http://localhost:7860
+  
+
+We use 'BlipProcessor' and 'BlipForConditionalGeneration' from the 'transformers' library to set up an image captioning model.  The 'flagged' folder containes the saved outputs from the web applications.
+<!-- For [TorchCaptioner.py](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/TorchCaptioner.py), the torchvision model is used. -->
+
+
+
+<!-- ![Image of me riding a tractor](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/test_data/Results/ScreenshotResult_1.png) -->
+
+![Image of me holding an old coffee mill](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/test_data/Results/ScreenshotResult_2.png)
+
+
+![Lion MisID](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/test_data/Results/ScreenshotResult_3.png)
+Figure 2: Visual interface of [TorchCaptioner.py](https://github.com/MartinezAgullo/GenAI_1_ImgCaptioner/blob/main/TorchCaptioner.py).
--- TorchCaptioner.py
+++ TorchCaptioner.py
@@ -1,21 +1,59 @@
 #######################################################################
-# TorchCaptioner.py 
+# TorchCaptioner.py :: Image Classification in PyTorch
 #     - Using a pretrained Resnet-18 model
+#     - Using Gradio as web interface
 #######################################################################
 
 import torch
-model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
-
 import requests
 from PIL import Image
 from torchvision import transforms
-# Download human-readable labels for ImageNet.
-response = requests.get("https://git.io/JJkYN")
-labels = response.text.split("\n")
+import gradio as gr
 
+#model = None
+#labels = None
+
+#######################################################################
+# main()
+#######################################################################
+
+def main():
+    global model, labels
+    model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
+    if model is None:
+        raise ValueError("Error al cargar el modelo ResNet-18.")
+
+    
+    # Download human-readable labels for ImageNet.
+    response = requests.get("https://git.io/JJkYN")
+    if response.status_code != 200:
+        raise ValueError("Error al descargar las etiquetas de ImageNet.")
+    labels = response.text.split("\n")
+
+    # Create gradio interface
+    gr.Interface(fn=predict,
+       inputs=gr.Image(type="pil"),
+       outputs=gr.Label(num_top_classes=3),
+       examples=["/test_data/lion.jpg", "/test_data/cheetah.jpg"]).launch()
+
+#######################################################################
+# predict()  
+#  1 - Converts the input image into a PIL Image and subsequently into a PyTorch tensor
+#  2 - Process the tensor through the MLmodel and returns the predictions in the dict confidences
+#######################################################################
 def predict(inp):
-   inp = transforms.ToTensor()(inp).unsqueeze(0)
-   with torch.no_grad():
-    prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
-    confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
-   return confidences
+    inp = transforms.ToTensor()(inp).unsqueeze(0)
+    global model, labels
+    if model is None:
+        raise ValueError("El modelo no está cargado correctamente.")
+    
+    with torch.no_grad():
+        prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
+        confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
+        # keys are the class labels
+        # values are the corresponding confidence probabilities (calculated with softmax)
+    return confidences
+
+
+if __name__ == '__main__':
+    main()
